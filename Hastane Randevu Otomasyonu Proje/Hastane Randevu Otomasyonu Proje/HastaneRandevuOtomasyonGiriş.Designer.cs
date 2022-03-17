5ba036f44f1a03e94724df2bf4765371c595e851
Cem Tekulus <dzhem326@gmail.com>
2022-03-17 12:21:30 +0300

    Create HastaneRandevuOtomasyonGiriş.Designer.cs

---
+++ Hastane Randevu Otomasyonu Proje/Hastane Randevu Otomasyonu Proje/HastaneRandevuOtomasyonGiriş.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace Hastane_Randevu_Otomasyonu_Proje
+{
+    partial class HastaneRandevuOtomasyonGiriş
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HastaneRandevuOtomasyonGiriş));
+            this.btn_admin = new System.Windows.Forms.Button();
+            this.btn_doktor = new System.Windows.Forms.Button();
+            this.btn_vatandas = new System.Windows.Forms.Button();
+            this.pb_182 = new System.Windows.Forms.PictureBox();
+            this.pb_sağlık = new System.Windows.Forms.PictureBox();
+            this.lb_admin = new System.Windows.Forms.Label();
+            this.lb_doktor = new System.Windows.Forms.Label();
+            this.lb_vatandaş = new System.Windows.Forms.Label();
+            this.lb_giriş = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_182)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_sağlık)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_admin
+            // 
+            this.btn_admin.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_admin.BackgroundImage")));
+            this.btn_admin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_admin.Location = new System.Drawing.Point(432, 227);
+            this.btn_admin.Name = "btn_admin";
+            this.btn_admin.Size = new System.Drawing.Size(100, 89);
+            this.btn_admin.TabIndex = 20;
+            this.btn_admin.UseVisualStyleBackColor = true;
+            this.btn_admin.Click += new System.EventHandler(this.btn_admin_Click);
+            // 
+            // btn_doktor
+            // 
+            this.btn_doktor.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_doktor.BackgroundImage")));
+            this.btn_doktor.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_doktor.Location = new System.Drawing.Point(247, 227);
+            this.btn_doktor.Name = "btn_doktor";
+            this.btn_doktor.Size = new System.Drawing.Size(100, 89);
+            this.btn_doktor.TabIndex = 19;
+            this.btn_doktor.UseVisualStyleBackColor = true;
+            this.btn_doktor.Click += new System.EventHandler(this.btn_doktor_Click);
+            // 
+            // btn_vatandas
+            // 
+            this.btn_vatandas.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_vatandas.BackgroundImage")));
+            this.btn_vatandas.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_vatandas.Location = new System.Drawing.Point(46, 227);
+            this.btn_vatandas.Name = "btn_vatandas";
+            this.btn_vatandas.Size = new System.Drawing.Size(113, 89);
+            this.btn_vatandas.TabIndex = 18;
+            this.btn_vatandas.UseVisualStyleBackColor = true;
+            this.btn_vatandas.Click += new System.EventHandler(this.btn_vatandas_Click);
+            // 
+            // pb_182
+            // 
+            this.pb_182.Image = ((System.Drawing.Image)(resources.GetObject("pb_182.Image")));
+            this.pb_182.Location = new System.Drawing.Point(40, 35);
+            this.pb_182.Name = "pb_182";
+            this.pb_182.Size = new System.Drawing.Size(100, 83);
+            this.pb_182.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_182.TabIndex = 17;
+            this.pb_182.TabStop = false;
+            // 
+            // pb_sağlık
+            // 
+            this.pb_sağlık.Image = ((System.Drawing.Image)(resources.GetObject("pb_sağlık.Image")));
+            this.pb_sağlık.Location = new System.Drawing.Point(449, 35);
+            this.pb_sağlık.Name = "pb_sağlık";
+            this.pb_sağlık.Size = new System.Drawing.Size(100, 82);
+            this.pb_sağlık.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_sağlık.TabIndex = 16;
+            this.pb_sağlık.TabStop = false;
+            // 
+            // lb_admin
+            // 
+            this.lb_admin.AutoSize = true;
+            this.lb_admin.Location = new System.Drawing.Point(427, 176);
+            this.lb_admin.Name = "lb_admin";
+            this.lb_admin.Size = new System.Drawing.Size(112, 25);
+            this.lb_admin.TabIndex = 15;
+            this.lb_admin.Text = "Admin Giriş";
+            // 
+            // lb_doktor
+            // 
+            this.lb_doktor.AutoSize = true;
+            this.lb_doktor.Location = new System.Drawing.Point(242, 176);
+            this.lb_doktor.Name = "lb_doktor";
+            this.lb_doktor.Size = new System.Drawing.Size(113, 25);
+            this.lb_doktor.TabIndex = 14;
+            this.lb_doktor.Text = "Doktor Giriş";
+            // 
+            // lb_vatandaş
+            // 
+            this.lb_vatandaş.AutoSize = true;
+            this.lb_vatandaş.Location = new System.Drawing.Point(41, 176);
+            this.lb_vatandaş.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_vatandaş.Name = "lb_vatandaş";
+            this.lb_vatandaş.Size = new System.Drawing.Size(140, 25);
+            this.lb_vatandaş.TabIndex = 13;
+            this.lb_vatandaş.Text = "Vatandaş Giriş";
+            // 
+            // lb_giriş
+            // 
+            this.lb_giriş.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lb_giriş.Location = new System.Drawing.Point(191, 64);
+            this.lb_giriş.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_giriş.Name = "lb_giriş";
+            this.lb_giriş.Size = new System.Drawing.Size(236, 34);
+            this.lb_giriş.TabIndex = 12;
+            this.lb_giriş.Text = "Hastane Giriş Ekranı";
+            // 
+            // HastaneRandevuOtomasyonGiriş
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 349);
+            this.Controls.Add(this.btn_admin);
+            this.Controls.Add(this.btn_doktor);
+            this.Controls.Add(this.btn_vatandas);
+            this.Controls.Add(this.pb_182);
+            this.Controls.Add(this.pb_sağlık);
+            this.Controls.Add(this.lb_admin);
+            this.Controls.Add(this.lb_doktor);
+            this.Controls.Add(this.lb_vatandaş);
+            this.Controls.Add(this.lb_giriş);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "HastaneRandevuOtomasyonGiriş";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pb_182)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_sağlık)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_admin;
+        private System.Windows.Forms.Button btn_doktor;
+        private System.Windows.Forms.Button btn_vatandas;
+        private System.Windows.Forms.PictureBox pb_182;
+        private System.Windows.Forms.PictureBox pb_sağlık;
+        private System.Windows.Forms.Label lb_admin;
+        private System.Windows.Forms.Label lb_doktor;
+        private System.Windows.Forms.Label lb_vatandaş;
+        private System.Windows.Forms.Label lb_giriş;
+    }
+}
+
