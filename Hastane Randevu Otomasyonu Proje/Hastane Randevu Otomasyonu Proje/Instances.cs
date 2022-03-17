2458cca4f890411c9d6e3c94fbf878abc15b57d8
Cem Tekulus <dzhem326@gmail.com>
2022-03-17 12:21:35 +0300

    Create Instances.cs

---
+++ Hastane Randevu Otomasyonu Proje/Hastane Randevu Otomasyonu Proje/Instances.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hastane_Randevu_Otomasyonu_Proje
+{
+    public static class Instances
+    {
+            public const string path = "Provider = Microsoft.Jet.OLEDB.4.0; Data Source =| DataDirectory |\\hastane.mdb";
+    
+    }
+}
